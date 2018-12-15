a9e7fe332186b738256c7f3d89268551a57e3ac9
bobbywalsh <bobbywalsh707@gmail.com>
2018-12-15 17:36:46 -0500

    Update MyBot-v1.py
    moved safeCheckMove into navigate ship
    
    call findHalite and gpsHome before navigate to find a target
    
    ships have an issues of moving between two points until exhaust.

--- MyBot-v1.py
+++ MyBot-v1.py
@@ -19,33 +19,31 @@ game = hlt.Game()
 ship_status = {}
 ship_target = {}
 
+def navigateShip():
+    if safeMoveCheck():
+        command_queue.append(ship.move(game_map.naive_navigate(ship, ship_target[ship.id])))
+    else:
+        command_queue.append(ship.move(Direction.invert(game_map.naive_navigate(ship, ship_target[ship.id]))))
 
-def safeMoveCheck(ship):
+
+def safeMoveCheck():
+    safe = bool(1)
     for x in range(-5 , 5, 1):
         for y in range(-5 , 5, 1):
             if game_map[Position(x, y)].is_occupied:
                 if game_map.calculate_distance(ship.position, Position(x, y)) <= 2.0:
-                    command_queue.append(ship.move(Direction.invert(game_map.naive_navigate(ship, Position(x, y)))))
+                    safe = bool(0)
+                    ship_target[ship.id] = Position(x, y)
+    return safe
 
-def findHalite(ship):
-    #find halite in forloop compare with current
-    #if better replace move
-    #else done move
+def findHalite():
     for x in range(-2 , 2, 1):
         for y in range(-2 , 2, 1):
             if game_map[ship.position.directional_offset((x,y))].halite_amount > game_map[ship_target[ship.id]].halite_amount: #and game_map[ship.position.directional_offset((x,y))] not in ship_target:
                 ship_target[ship.id] = ship.position.directional_offset((x,y))
-                #map = game_map.naive_navigate(ship, ship_target[ship.id])
-            #else:
-                #map = game_map.naive_navigate(ship, ship_target[ship.id])
-                #logging.info("Ship {} has {} target.".format(ship.id, ship_target[ship.id]))
 
-    command_queue.append(ship.move(game_map.naive_navigate(ship, ship_target[ship.id])))
-
-def gpsHome(ship):
+def gpsHome():
     ship_target[ship.id] = me.shipyard.position
-    command_queue.append(ship.move(game_map.naive_navigate(ship, ship_target[ship.id])))
-
 
 
 # Respond with your name.
@@ -61,42 +59,35 @@ while True:
     # A command queue holds all the commands you will run this turn.
     command_queue = []
 
+    # Spawn ship if halite is over 3000
+    if not game_map[me.shipyard].is_occupied and me.halite_amount >= 2000:
+       command_queue.append(me.shipyard.spawn())
+
+
     for ship in me.get_ships():
         if ship.id not in ship_status:
             ship_status[ship.id] = "exploring"              #default status
             ship_target[ship.id] = me.shipyard.position     #default target
-            safeMoveCheck(ship)                             #check surroundings
-            findHalite(ship)                                #find best halite and move
-            continue
+            findHalite()
+            navigateShip()
         elif ship_status[ship.id] == "exploring":
             if ship.halite_amount >= 700:
-                ship_status[ship.id] = "returning"
-                safeMoveCheck(ship)
-                gpsHome(ship)
-                continue
+                ship_status[ship.id] = "returning"              #default status
+                gpsHome()
+                navigateShip()
             else:
-                safeMoveCheck(ship)                             #check surroundings
-                findHalite(ship)                                #find best halite and move
-                #print(ship_target[ship.id])
-        elif ship_status[ship.id] == "returning":
-            if ship.position == me.shipyard.position:
-                ship_status[ship.id] = "exploring"
-                safeMoveCheck(ship)                             #check surroundings
-                findHalite(ship)                                #find best halite and move
-            else:
-                gpsHome(ship)
-                continue
+                findHalite()
+                navigateShip()
         else:
-            ship_status[ship.id] = "exploring"
-            gpsHome(ship)
-            continue
+            if ship.position == me.shipyard.position:
+                ship_status[ship.id] = "exploring"              #default status
+                findHalite()
+                navigateShip()
+            else:
+                #ship_status[ship.id] = "returning"              #default status
+                gpsHome()
+                navigateShip()
 
-
-
-    #Spawn ship at 5000 or more
-    if not game_map[me.shipyard].is_occupied and me.halite_amount >= 1001:
-       command_queue.append(me.shipyard.spawn())
-      #logging.info("Ship spawned{}")
 
     # Send your moves back to the game environment, ending this turn.
     game.end_turn(command_queue)
